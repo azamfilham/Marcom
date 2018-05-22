4c5f763d0546ec45f8849575f667f43b6c02fb6d
ftreciana <freeskatreciana@gmail.com>
2018-05-22 15:24:43 +0700

    role

--- Code/Marcom/Marcom.Repository/M_RoleRepo.cs
+++ Code/Marcom/Marcom.Repository/M_RoleRepo.cs
@@ -16,6 +16,7 @@ namespace Marcom.Repository
             using (var db = new MarcomContext())
             {
                 result = (from r in db.m_role
+                          where r.is_delete == false
                           select new M_RoleViewModel
                           {
                               Id = r.id,
@@ -31,6 +32,7 @@ namespace Marcom.Repository
             }
             return result;
         }
+
         public static M_RoleViewModel GetById(int id)
         {
             M_RoleViewModel result = new M_RoleViewModel();
@@ -52,6 +54,114 @@ namespace Marcom.Repository
                           }).FirstOrDefault();
             }
             return result;
+        }
+
+        public static Responses Update(M_RoleViewModel entity)
+        {
+            Responses result = new Responses();
+            try
+            {
+                using (var db = new MarcomContext())
+                {
+                    if (entity.Id != 0)
+                    {
+                        m_role role = db.m_role.Where(o => o.id == entity.Id).FirstOrDefault();
+                        if (role != null)
+                        {
+                            role.code = entity.Code;
+                            role.name = entity.Name;
+                            role.description = entity.Description;
+                            role.is_delete = entity.IsDelete;
+                            role.updated_by = "Freeska";
+                            role.updated_date = DateTime.Now;
+                            db.SaveChanges();
+                        }
+                    }
+                    else
+                    {
+                        m_role role = new m_role();
+                        role.code = entity.Code;
+                        role.name = entity.Name;
+                        role.description = entity.Description;
+                        role.is_delete = entity.IsDelete;
+                        role.created_by = "Freeska";
+                        role.created_date = DateTime.Now;
+                        db.m_role.Add(role);
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+                result.Success = false;
+            }
+            return result;
+        }
+
+        public static Responses Delete(int id)
+        {
+            Responses result = new Responses();
+            try
+            {
+                using (var db = new MarcomContext())
+                {
+                    m_role role = db.m_role.Where(o => o.id == id).FirstOrDefault();
+                    if (role != null)
+                    {
+                        role.is_delete = true;
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+                result.Success = false;
+            }
+            return result;
+        }
+
+        public static string GetNewCode()
+        {
+            
+
+            int newIncrement = 1;
+            string newCode = string.Format("RO");
+            using (var db = new MarcomContext())
+            {
+                var result = (from r in db.m_role
+                              where r.code.Contains(newCode)
+                              select new { code = r.code })
+                              .OrderByDescending(o => o.code)
+                              .FirstOrDefault();
+                if (result != null)
+                {
+                    string[] oldCode = SplitCode(result.code);
+                    newIncrement = int.Parse(oldCode[0]) + 1;
+                }
+
+            }
+            newCode += newIncrement.ToString("D4");
+            return newCode;
+        }
+
+        public static string[] SplitCode(string data)
+        {
+            string numbers = "";
+            string alpha = "";
+            foreach (char c in data)
+            {
+                if (Char.IsDigit(c))
+                {
+                    numbers = numbers + c;
+                }
+                else if (Char.IsLetter(c))
+                {
+                    alpha = alpha + c;
+                }
+            }
+            return new string[] { numbers, alpha };
         }
     }
 }
--- Code/Marcom/Marcom.ViewModel/M_RoleViewModel.cs
+++ Code/Marcom/Marcom.ViewModel/M_RoleViewModel.cs
@@ -9,22 +9,26 @@ namespace Marcom.ViewModel
 {
     public class M_RoleViewModel
     {
+        public M_RoleViewModel()
+        {
+            IsDelete = false;
+        }
         public int Id { get; set; }
 
-        [Display(Name = "Role Code")]
+        [Display(Name ="Role Code"), Required]
         public string Code { get; set; }
 
-        [Display(Name = "Role Name")]
+        [Display(Name = "Role Name"), Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public bool IsDelete { get; set; }
 
-        [Display(Name = "Created By")]
+        [Display(Name = "Created By"), Required]
         public string CreatedBy { get; set; }
 
-        [Display(Name = "Created Date")]
+        [Display(Name = "Created Date"), DisplayFormat(DataFormatString = "{0:dd MMM yyyy}"), Required]
         public DateTime CreatedDate { get; set; }
 
         public string UpdatedBy { get; set; }
