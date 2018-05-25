3740054312400ab25d2207f01c66cc0718e7dfdc
azamfilham <azamfilham@gmail.com>
2018-05-25 16:27:05 +0700

    coba commit

--- Code/Marcom/Marcom.MVC/Controllers/T_EventController.cs
+++ Code/Marcom/Marcom.MVC/Controllers/T_EventController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Marcom.Repository;
+using Marcom.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +18,19 @@ namespace Marcom.MVC.Controllers
 
         public ActionResult Create()
         {
-            return PartialView("_Create");
+            T_EventViewModel model = new T_EventViewModel();
+            model.Code = T_EventRepo.GetNewCode();
+            return PartialView("_Create", model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return PartialView("_Edit", T_EventRepo.GetById(id));
+        }
+
+        public ActionResult View(int id)
+        {
+            return PartialView("_View", T_EventRepo.GetById(id));
         }
     }
 }
