cbf05c006b862f97d8884603be298c925087c1c1
mohasyam <asyam.mohammad@gmail.com>
2018-05-28 08:24:06 +0700

    update tdesignitemscontroller

--- Code/Marcom/Marcom.API/Controllers/T_DesignItemsController.cs
+++ Code/Marcom/Marcom.API/Controllers/T_DesignItemsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Marcom.Repository;
+using Marcom.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,15 +12,21 @@ namespace Marcom.API.Controllers
     public class T_DesignItemsController : ApiController
     {
         // GET: api/T_DesignItems
-        public IEnumerable<string> Get()
+        public IEnumerable<T_DesignItemViewModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            return T_DesignItemRepo.Get();
         }
 
         // GET: api/T_DesignItems/5
         public string Get(int id)
         {
             return "value";
+        }
+
+        [HttpGet]
+        public IEnumerable<T_DesignItemViewModel> GetByDesign(int DesignId)
+        {
+            return T_DesignItemRepo.GetByDesignId(DesignId);
         }
 
         // POST: api/T_DesignItems
